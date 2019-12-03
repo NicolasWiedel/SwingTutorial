5a7b4274bd3f5b8c0598b011d1f6e82131566a26
Nicolas <nicolaswiedel@googlemail.com>
2019-12-03 18:17:51 +0100

    Abschluß Kapitel 5

--- SwingTutorial/src/Kapitel_4_5/SwingLoginExaple.java
+++ SwingTutorial/src/Kapitel_4_5/SwingLoginExaple.java
@@ -6,6 +6,8 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -15,10 +17,12 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 
-public class SwingLoginExaple {
+public class SwingLoginExaple implements ActionListener, KeyListener{
 
 	private JFrame jFrame;
 	private Container contentPane; 
@@ -31,6 +35,9 @@ public class SwingLoginExaple {
 	private JButton btnReset;
 	private JCheckBox cbShowPassword;
 	private JCheckBox cbHidePassword;
+	
+	private final String usernamString = "nwiedel";
+	private final String passwordFieldString = "Vio310567";
 	
 	public SwingLoginExaple(){
 		initUI();
@@ -73,6 +80,8 @@ public class SwingLoginExaple {
 		exitItem.addActionListener(e -> {
 			System.exit(0);
 		});
+		exitItem.setMnemonic(KeyEvent.VK_E);
+		exitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, KeyEvent.ALT_MASK));
 		
 		fileMenu.add(exitItem);
 	}
@@ -83,11 +92,15 @@ public class SwingLoginExaple {
 		
 		tfUser = new JTextField();
 		pwfPassword = new JPasswordField();
+		pwfPassword.addKeyListener(this);
 		
 		btnLogin = new JButton("Login");
+		btnLogin.addActionListener(this);
 		btnReset = new JButton("Reset");
+		btnReset.addActionListener(this);
 		
 		cbShowPassword = new JCheckBox("Passwort anzeigen");
+		cbShowPassword.addActionListener(this);
 		cbHidePassword = new JCheckBox("Passwort verbergen");
 	}
 	
@@ -96,7 +109,7 @@ public class SwingLoginExaple {
 		contentPane.add(lblPassword);
 		contentPane.add(tfUser);
 		contentPane.add(pwfPassword);
-		contentPane.add(cbHidePassword);
+//		contentPane.add(cbHidePassword);
 		contentPane.add(cbShowPassword);
 		contentPane.add(btnLogin);
 		contentPane.add(btnReset);
@@ -120,6 +133,61 @@ public class SwingLoginExaple {
 	
 	}
 	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == btnLogin){
+			String userSting = tfUser.getText();
+			char[] chars = pwfPassword.getPassword();
+			String passwordString = new String(chars);
+			
+			if(usernamString.equalsIgnoreCase(userSting) 
+					&& passwordFieldString.equalsIgnoreCase(passwordString)){
+				System.out.println("Login erfolgreich!");
+				createDialog("Login erfolgreich!");
+			} else {
+				System.out.println("Username oder Passwort falsch!");
+				createDialog("Username oder Passwort falsch!");
+			}
+		}
+		
+		if(e.getSource() == btnReset){
+			tfUser.setText("");
+			pwfPassword.setText("");
+		}
+		
+		if(e.getSource() == cbShowPassword){
+			if(cbShowPassword.isSelected()){
+				pwfPassword.setEchoChar((char)0);
+			} else {
+				pwfPassword.setEchoChar('*');
+			}
+		}	
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_ENTER){
+			System.out.println("Zum Login!");
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void createDialog(String message){
+		JOptionPane.showMessageDialog(jFrame, message);
+	}
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			
@@ -129,4 +197,5 @@ public class SwingLoginExaple {
 			}
 		});
 	}
+
 }
